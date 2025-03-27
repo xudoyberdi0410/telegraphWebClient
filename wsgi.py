6ab0a99962e24c23ec009da203c4f448ca99bad1
xudoyberdi0410 <xudoyberdi0410@gmail.com>
2025-03-27 22:56:58 +0500

    Update wsgi.py to use environment variable for port configuration

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
 from app.main import app
- 
+import os
+
+PORT = int(os.getenv("PORT", 10000))  # Render передаёт порт через переменную окружения
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=PORT)
